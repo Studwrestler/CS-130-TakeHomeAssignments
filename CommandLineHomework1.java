1a78aa74f16b0de8f3e80859d659c8fefe76193a
Studwrestler <112411998+Studwrestler@users.noreply.github.com>
2022-09-07 11:54:31 -0400

    Add files via upload

--- CommandLineHomework1.java
+++ CommandLineHomework1.java
@@ -4,10 +4,8 @@ public class CommandLineHomework1
 	{
 		for(String str:args)
 		{
-			System.out.println("My first name is :" + args[0]);
-			System.out.println("My last name is :" + args[1]);
-			System.out.println("A hobby of mine is :" + args[2]);
-			System.out.println("My major is :" + args[3]);
+			System.out.println("My first name is :" + str);
+		;
 		}
 	}
 }
